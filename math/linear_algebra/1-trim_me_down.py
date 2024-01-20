fdf0dc0ff567e5855c955db57a2a89201aab58a0
rumunyana <113432535+rumunyana@users.noreply.github.com>
2024-01-20 12:50:28 +0200

    changes

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
-arr1 = arr[0:2]
-arr2 = arr[4:]
-arr3 = arr[1:6]
-print("The first two numbers of the array are: {}".format(arr1))
-print("The last five numbers of the array are: {}".format(arr2))
-print("The 2nd through 6th numbers of the array are: {}".format(arr3))
+matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
+the_middle = []
+for i in range(len(matrix)):
+    the_middle.append(matrix[i][2:4])
+print("The middle columns of the matrix are: {}".format(the_middle))
