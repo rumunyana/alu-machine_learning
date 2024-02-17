58499415c37cae0d198512d01b315e67876d6ee0
rumunyana <113432535+rumunyana@users.noreply.github.com>
2024-02-17 22:23:31 +0200

    changes

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -1,1 +1,45 @@
 #!/usr/bin/env python3
+"""
+Defines a function that performs convolution
+on a grayscale image with given padding and stride
+"""
+
+
+import numpy as np
+
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """
+    Performs a convolution on grayscale images with given padding and stride
+    returns:
+        numpy.ndarray contained convolved images
+    """
+    m = images.shape[0]
+    height = images.shape[1]
+    width = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+    sh, sw = stride
+    if padding is 'same':
+        ph = ((((height - 1) * sh) + kh - height) // 2) + 1
+        pw = ((((width - 1) * sw) + kw - width) // 2) + 1
+    elif padding is 'valid':
+        ph = 0
+        pw = 0
+    else:
+        ph, pw = padding
+    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
+                    'constant', constant_values=0)
+    ch = ((height + (2 * ph) - kh) // sh) + 1
+    cw = ((width + (2 * pw) - kw) // sw) + 1
+    convoluted = np.zeros((m, ch, cw))
+    i = 0
+    for h in range(0, (height + (2 * ph) - kh + 1), sh):
+        j = 0
+        for w in range(0, (width + (2 * pw) - kw + 1), sw):
+            output = np.sum(images[:, h: h + kh, w: w + kw] * kernel,
+                            axis=1).sum(axis=1)
+            convoluted[:, i, j] = output
+            j += 1
+        i += 1
+    return convoluted
