8cf87466dd49241a02dee5042eb77c16a2c72d91
r.umunyana <r.umunyana@alustudent.com>
2024-06-30 16:25:21 +0200

    fixes

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 By using the GitHub API, write a script that prints
 the location of a specific user:
@@ -19,17 +19,28 @@ def main(url):
     - Your code should not be executed when the file is
     imported (you should use if __name__ == '__main__':)
     """
-    response = requests.get(url)
+    try:
+        print("Sending request to:", url)
+        response = requests.get(url, timeout=10)  # Add a timeout to the request
+        response.raise_for_status()  # Raise an error for bad HTTP status codes
+        print("Response received with status code:", response.status_code)
+    except requests.exceptions.Timeout:
+        print("Request timed out")
+        sys.exit(1)
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+        sys.exit(1)
 
     if response.status_code == 404:
         print("Not found")
     elif response.status_code == 403:
-        reset_timestamp = int(response.headers["X-Ratelimit-Reset"])
+        reset_timestamp = int(response.headers.get("X-Ratelimit-Reset", 0))
         current_timestamp = int(time.time())
         reset_in_minutes = (reset_timestamp - current_timestamp) // 60
         print("Reset in {} min".format(reset_in_minutes))
     else:
         user_data = response.json()
+        print("Full JSON response:", user_data)  # Print the full JSON response
         location = user_data.get("location")
         if location:
             print(location)
@@ -38,7 +49,8 @@ def main(url):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python 2-user_location.py <GitHub API URL>")
+        print("Usage: ./2-user_location.py <GitHub API URL>")
         sys.exit(1)
     
     main(sys.argv[1])
+
