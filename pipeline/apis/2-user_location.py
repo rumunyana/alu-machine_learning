eb59871c55b613fa397180e7ef17ddb9360836cd
r.umunyana <r.umunyana@alustudent.com>
2024-06-30 18:56:12 +0200

    fixes

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python3
-"""
-    This module returns the location of a user
-    with a GitHub API URL.
-"""
-import requests
+'''scrap from githib'''
+
+
 import sys
+import requests
 import time
 
 
 def get_user_location(api_url):
-    """Returns the user's location"""
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        user_data = response.json()
-        print(user_data.get('location', 'Location not specified'))
-    elif response.status_code == 403:
-        reset_time = int(response.headers.get('X-RateLimit-Reset', 0))
-        reset = (reset_time - time.time()) / 60
-        print('Reset in {} min'.format(reset))
-    elif response.status_code == 404:
-        print('Not found')
-    else:
-        print('Error: {}'.format(response.status_code))
+    '''return user's location'''
+    try:
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            user_data = response.json()
+            print(user_data.get('location', 'Location not specified'))
+        elif response.status_code == 404:
+            print('Not found')
+        elif response.status_code == 403:
+            reset_time = int(response.headers.get('X-RateLimit-Reset', 0))
+            reset_in = (reset_time - time.time()) / 60
+            print('Reset in {} min'.format(int(reset_in)))
+        else:
+            print('Error: {}'.format(response.status_code))
+    except requests.exceptions.RequestException as e:
+        print('Request failed: {}'.format(e))
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Usage: ./2-user_location.py <API_URL>')
     else:
-        get_user_location(sys.argv[1])
+        api_url = sys.argv[1]
+        get_user_location(api_url)
